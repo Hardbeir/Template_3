277997209a777b30ed7befa315557d7be6c5e52a
vadym chernichov <mracdorac@gmail.com>
2021-12-21 19:59:40 +0100

    finish template_3

--- source/js/script.js
+++ source/js/script.js
@@ -116,3 +116,28 @@ var swiper = new Swiper(".blogs-slider", {
         },
     },
 });
+
+
+var swiper = new Swiper(".clients-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        640: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            slidesPerView: 4,
+        },
+    },
+});
